18e8aeb73cfa961b12381f2fb3487e4edbf62856
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-02-18 17:40:39 +0100

    update X button

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -22,63 +22,71 @@ function launchModal() {
 
 
 function validate() {
-      
+      let isValid = true;
   if( document.reserve.first.value == "" ) {
      missFirst.textContent = "Prénom manquant!"
      missFirst.style.color = "red"
      document.reserve.first.focus() ;
-     return false;
+     isValid = false;
   }
   if( document.reserve.last.value == "" ) {
      missLast.textContent = "Nom manquant!"
      missLast.style.color = "red"
      document.reserve.last.focus() ;
-     return false;
+     isValid = false;
   }
   if( document.reserve.email.value == "" ) {
      missEmail.textContent = "Email manquant!"
      missEmail.style.color = "red"
      document.reserve.email.focus() ;
-     return false;
+     isValid = false;
      }
      if( document.reserve.birthdate.value == "" ) {
       missBirth.textContent = "Date de naissance manquante!"
       missBirth.style.color = "red"
       document.reserve.birthdate.focus() ;
-      return false;
+      isValid = false;
      }
      if( document.reserve.quantity.value == "" ) {
       missQuantity.textContent = "Nombre de participation manquant!"
       missQuantity.style.color = "red"
       document.reserve.quantity.focus() ;
-      return false;
+      isValid = false;
      }
      if( document.reserve.location.value == "" ) {
         missLocation.textContent = "Veuillez choisir une ville!"
         missLocation.style.color = "red"
         document.reserve.location.focus() ;
-        return false;
+        isValid = false;
      }
      if( document.reserve.checkboxreq.value == "" ) {
         missCheck.textContent = "Veuillez accepter les conditions!"
         missCheck.style.color = "red"
         document.reserve.checkboxreq.focus() ;
-        return false;
+        isValid = false;
      }
-  return( true );
+  return isValid;
 }
 
-/*let form = document.getElementsByTagName("form")[0];
+let form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  if (validate()){
+   modalbg.style.display = "none",
   alert("Formulaire envoyé!");
-});*/
+  }
+});
 
-function validateForm(){
+document.getElementById('closeButton').addEventListener('click', (event) => {
+event.preventDefault();
+modalbg.style.display = 'none';
+});
+
+/*function validateForm(){
 
 
-var first = document.form['reserve']['first'].value;
-var last = document.form['reserve']['last'].value;
+var first = document.reserve['reserve']['first'].value;
+var last = document.reserve['reserve']['last'].value;
 
 if (first.length <= 3 || first.length >= 12) {
    alert('Your first name must be between 4 to 11 characters.');
@@ -87,4 +95,4 @@ if (first.length <= 3 || first.length >= 12) {
 if (last.length <= 3 || last.length >= 12) {
    alert('Your last name must be between 4 to 11 characters.');
 }
-}
+}*/
